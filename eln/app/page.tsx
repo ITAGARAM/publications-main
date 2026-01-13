341bf987b7418d1739645b2cf6702de83f017ee2
Koushik Karthikeyan P. <ate268@corpagaram.com>
2026-01-13 11:13:55 +0530

    menu rework

--- eln/app/page.tsx
+++ eln/app/page.tsx
@@ -2,13 +2,17 @@
 
 import { useEffect } from 'react';
 import { useRouter } from 'next/navigation';
+import Blog from './blog/page';
 
-export default function HomeRedirect() {
-  const router = useRouter();
+export default function Home() {
+  // const router = useRouter();
 
-  useEffect(() => {
-    router.push('/blog');
-  }, [router]);
+  // useEffect(() => {
+  //   router.push('/blog');
+  // }, [router]);
 
-  return null; // No visible content, just redirects instantly
+  // return null; // No visible content, just redirects instantly
+  return (
+    <Blog />
+  )
 }
