0d1b39491d24d427549a4a2176d96d1f1272498e
agaram916 <natashajilliankavi@gmail.com>
2026-03-10 16:15:01 +0530

    sitemap

--- eln/app/sitemap.xml/route.ts
+++ eln/app/sitemap.xml/route.ts
@@ -2,38 +2,65 @@ import { NextResponse } from "next/server";
 import { client } from "@/sanity/lib/client";
 
 export async function GET() {
+
   const baseUrl = "https://publications.agaramtech.com";
 
- const posts = await client.fetch(`
+  // Static pages
+  const staticUrls = `
+    <url>
+      <loc>${baseUrl}</loc>
+      <priority>1.0</priority>
+    </url>
+    <url>
+      <loc>${baseUrl}/publications</loc>
+      <priority>0.9</priority>
+    </url>
+  `;
+
+  // BLOG POSTS
+  const blogs = await client.fetch(`
   *[_type == "blog" && defined(slug.current)]{
     "slug": slug.current,
     publishedAt
   }
-`);
+  `);
 
+  const blogUrls = blogs.map((post:any) => `
+    <url>
+      <loc>${baseUrl}/${post.slug}</loc>
+      <lastmod>${post.publishedAt}</lastmod>
+      <priority>0.8</priority>
+    </url>
+  `).join("");
 
-  const urls = posts
-    .map(
-      (post: any) => `
-        <url>
-          <loc>${baseUrl}/${post.slug}</loc>
-          <lastmod>${post.publishedAt}</lastmod>
-          <changefreq>weekly</changefreq>
-          <priority>0.8</priority>
-        </url>
-      `
-    )
-    .join("");
+  // PUBLICATIONS
+  const publications = await client.fetch(`
+  *[_type == "publication" && defined(slug.current)]{
+    "slug": slug.current,
+    publishedAt
+  }
+  `);
+
+  const publicationUrls = publications.map((post:any) => `
+    <url>
+      <loc>${baseUrl}/publications/${post.slug}</loc>
+      <lastmod>${post.publishedAt}</lastmod>
+      <priority>0.8</priority>
+    </url>
+  `).join("");
 
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-${urls}
-</urlset>`;
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+
+  ${staticUrls}
+  ${blogUrls}
+  ${publicationUrls}
+
+  </urlset>`;
 
   return new NextResponse(sitemap, {
-    status: 200,
     headers: {
-      "Content-Type": "application/xml; charset=utf-8",
+      "Content-Type": "application/xml",
     },
   });
 }
