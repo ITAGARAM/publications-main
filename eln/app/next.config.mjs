07eb35709790ec159bfee4c1084a991b1d4b6113
agaram916 <natashajilliankavi@gmail.com>
2026-05-21 11:56:11 +0530

    redireaction 10

--- eln/app/next.config.mjs
+++ eln/app/next.config.mjs
@@ -1,30 +1,17 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-    trailingSlash: true,
+  trailingSlash: true,
 
-    images: {
-        unoptimized: true,
-    },
-
-    eslint: {
-        ignoreDuringBuilds: true,
-    },
-
-    async redirects() {
-        return [
-            {
-                source: '/',
-                destination: 'https://www.agaramtech.com/blog/',
-                permanent: true,
-            },
-            {
-                source: '/:slug/',
-                destination: 'https://www.agaramtech.com/blog/:slug/',
-                permanent: true,
-            },
-        ];
-    },
+  async redirects() {
+    return [
+      {
+        source: '/:slug/',
+        destination: 'https://www.agaramtech.com/blog/:slug/',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
