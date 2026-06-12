d94050dd929389099f93b851cc15aa1e29a4740c
agaram916 <natashajilliankavi@gmail.com>
2026-06-12 16:00:10 +0530

    blog

---
+++ eln/app/blog/[slug]/page.tsx
@@ -0,0 +1,9 @@
+import { redirect } from "next/navigation";
+
+export default function Page({
+  params,
+}: {
+  params: { slug: string };
+}) {
+  redirect(`https://www.agaramtech.com/blog/${params.slug}/`);
+}
